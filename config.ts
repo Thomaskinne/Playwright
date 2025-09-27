d8fe5999273169ec9e6c870063678a0727a15e13
Thomas Kinne <Thomaskinne@outlook.com>
2025-09-27 15:14:33 -0400

    updated config file and login page

---
+++ config.ts
@@ -0,0 +1,14 @@
+// config.ts
+
+// Login page URL
+export const LOGIN_URL = 'https://www.saucedemo.com/';
+
+// Users
+export const users = {
+  standard: { username: 'standard_user', password: 'secret_sauce' },
+  lockedOut: { username: 'locked_out_user', password: 'secret_sauce' },
+  problem: { username: 'problem_user', password: 'secret_sauce' },
+  performanceGlitch: { username: 'performance_glitch_user', password: 'secret_sauce' },
+  error: { username: 'error_user', password: 'secret_sauce' },
+  visual: { username: 'visual_user', password: 'secret_sauce' },
+};
